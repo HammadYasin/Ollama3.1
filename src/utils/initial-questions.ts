87f3ec117a06327a00d619a7bd8ccbc9065f7bb6
Hammad Yasin <hammadyasin817@gmail.com>
2024-07-30 09:59:21 +0300

    first commit

---
+++ src/utils/initial-questions.ts
@@ -0,0 +1,35 @@
+export const INITIAL_QUESTIONS = [
+  {
+    content: "Pakistan ka darul hukumat kya hai? Aur please, Islamabad hi bolna!",
+  },
+  {
+    content: "Allama Iqbal ne kis kitaab likhi thi? Shayad 'Shayari for Dummies' thi?",
+  },
+  {
+    content: "Pani ka ubalnay ka nuqtah kitne darjah Celsius par hai? Garmiyon mein, fridge mein bhi ubal jata hai!",
+  },
+  {
+    content: "Humare solar system mein kitnay sayyaray hain? Chalo, tumhare marks ke jitnay nahi, but kafi hain!",
+  },
+  {
+    content: "Pakistan ka kab waqoof hua tha? Ghalti se ya jaan bujh kar?",
+  },
+  {
+    content: "Mona Lisa ki tasveer kis ne banayi thi? Da Vinci ne, warna tu khud hi Mona banjata!",
+  },
+  {
+    content: "144 ka murabba jad kya hai? Agar maloom nahi, to maths teacher se dosti banalo!",
+  },
+  {
+    content: "Pakistan ka mojudah wazir-e-azam kaun hai? Jo bhi hai, abhi tak badla nahi?",
+  },
+  {
+    content: "Dunya ka sabse ooncha pahad kaunsa hai? K2 nahi, Mount Everest hai beta!",
+  },
+  {
+    content: "Sone ka chemical symbol kya hai? Nakhre mat kar, AU bol aur aagay badh!",
+  },
+  {
+    content: "Penicillin kis ne daryaft ki thi? Alexander Fleming, warna teri naak kabhi nahi khulti!",
+  },
+];
